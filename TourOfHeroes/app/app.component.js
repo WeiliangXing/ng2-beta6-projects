50d2331a532ca3c469b642d4ab39c1aca27b033c
wxing <xingwl0401@gmail.com>
2016-02-16 13:56:57 -0500

    two way binding of input data

--- TourOfHeroes/app/app.component.js
+++ TourOfHeroes/app/app.component.js
@@ -27,7 +27,7 @@ System.register(['angular2/core'], function(exports_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
+                        template: "\n\t  <h1>{{title}}</h1>\n\t  <h2>{{hero.name}} details!</h2>\n\t  <div><label>id: </label>{{hero.id}}</div>\n\t  <div>\n\t    <label>name: </label>\n\t    <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n\t  </div>\n\t  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
